9c0ce82ac6fe99248604a3f10ebf4d1d69391fb1
Dishon Davis <dishonodavis@gmail.com>
2019-07-29 07:03:43 -0400

    Add files via upload

---
+++ fitness-auth/config/database.js
@@ -0,0 +1,14 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://resilient:fproject@fitness-on5e6.mongodb.net/getFit?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'resilient'
+};
+
+
+    //cluster name = fitness
+    //mongodb = resilient
+    //mongodb password = fproject
+    //dbName =
+    //dbpassword =
+    //collection name = savage_prp
